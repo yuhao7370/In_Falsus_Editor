eb772475492d515b610fcfae505388c34ce17c17
yuhao7370 <53969724+yuhao7370@users.noreply.github.com>
2026-02-19 23:57:43 +0800

    多平台

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows" {
+    #[cfg(windows)]
+    {
         let mut res = winres::WindowsResource::new();
         res.set_icon("icon.ico");
         res.compile().expect("Failed to compile Windows resources");
